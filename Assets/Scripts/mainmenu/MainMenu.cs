3ef626270c9498fbe02c92588eb8fa5effcb4a2e
André B Gorzoni <andreboolandim2@gmail.com>
2025-11-27 17:51:26 -0300

    feat fx

--- Assets/Scripts/mainmenu/MainMenu.cs
+++ Assets/Scripts/mainmenu/MainMenu.cs
@@ -1,12 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject buttonsPanel;
+    [SerializeField] private GameObject optionsPanel;
+
     public void StartGame()
     {
         SceneManager.LoadScene("Basement");
     }
+
+    public void OpenOptions()
+    {
+        buttonsPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+        buttonsPanel.SetActive(true);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
